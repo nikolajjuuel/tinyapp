d5e28d6af6f6dbc5c8facfe61c05aabf521fcb15
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-09-28 01:04:37 +0000

    body parser install

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -22,14 +24,15 @@ app.get("/", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
   app.get("/urls/:shortURL", (req, res) => {
     const shortURL = req.params.shortURL;
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL]};
     res.render("urls_show", templateVars);
   });
-
-
-
 
   app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);

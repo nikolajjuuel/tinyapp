89c5ef338f8258f876509a80174ec16d21ae3b63
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-09-28 03:23:15 +0000

    short url generated and long url posted to urls

--- express_server.js
+++ express_server.js
@@ -6,6 +6,17 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+
+function generateRandomString() {
+  const options ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const urlShort = [];
+    for (let i = 0; i < 6; i++){
+      const random = Math.floor(Math.random() * options.length + 1);
+      urlShort.push(options[random])
+    }
+   return urlShort.join('');
+  }
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -39,19 +50,14 @@ app.get("/", (req, res) => {
   });
 
   app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+   // console.log(req.body);  // Log the POST request body to the console
+    const inputUrl = req.body.longURL;
+    const randomString = generateRandomString();
+    urlDatabase[randomString] = inputUrl;
+    console.log(urlDatabase);
+    res.redirect('/urls')
   });
 
-  function generateRandomString() {
-    const options ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const urlShort = [];
-      for (let i = 0; i < 6; i++){
-        const random = Math.floor(Math.random() * options.length + 1);
-        urlShort.push(options[random])
-      }
-     return urlShort.join('');
-    }
 
 
 app.listen(PORT, () => {
